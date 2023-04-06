ed6467560fada12c008bff2bb74da7334bf295cf
jeferson marciano <jefersonmarcian6@gmail.com>
2023-04-06 18:59:18 -0300

    comit de algoritmo simples de converção de c para f com comandos toFixed e parseFloat

---
+++ main.js
@@ -0,0 +1,9 @@
+ let celsius = 0
+ let fahrenheit = 0
+
+
+ celsius = parseFloat(prompt('digite a temperatura em celsius: '))
+ 
+ fahrenheit = celsius * 1.8 + 32
+ 
+ alert(`A temperatura em fahrenheit é de ${fahrenheit.toFixed(2)}`)
